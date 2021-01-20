c2d80967f5b6da718765952086233d62e5be8d19
Zeca Figueiredo <zecafig@me.com>
2021-01-20 14:07:43 -0300

    First commit

---
+++ objectid_convert.py
@@ -0,0 +1,9 @@
+"""
+Convert an ObjectId in to some short types.
+
+>>> objectid_convert_string_8(5e596e5f050f17fcca2a032)
+abcD123$
+
+>>> objectid_convert_int_8(5e596e5f050f17fcca2a032)
+12345678
+"""
